cd4e30edc6de8330ae46bccae3193dd424676bec
superdun <475098936@qq.com>
2016-07-01 11:27:10 +0000

    ##

--- instance/localConfig.py
+++ instance/localConfig.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
 
-SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:password@localhost:3306/internBlog'
+SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:internsBlogdb@localhost:3306/internBlog'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
